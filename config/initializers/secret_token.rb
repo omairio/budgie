f860147cd759ddd873d5e449d66ed5d0463cf75d
Omair Ali <omairoa@gmail.com>
2014-10-08 01:40:12 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budgie::Application.config.secret_key_base = '4e2775fc9e7a14b2e0b02099914f7a5b92012aae088afca9b7065d1c6389fd446af10355997559dd0eed11d07f38296605ac706ea39c2fa62f28972161dec477'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Budgie::Application.config.session_store :cookie_store, key: '_budgie_session'
