64fe33a0c5051e5640880da3fdd403f3151d8654
Omair <omairoa@gmail.com>
2014-10-09 13:43:40 +1100

    transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,31 @@
 class Transaction < ActiveRecord::Base
+	validates(:amount, { 
+		presence: true, 
+		})
+	validates(:user_id, { 
+		presence: true, 
+		})
+
+	validates(:date, { 
+		presence: true, 
+		})
+
+# Day spread has a max value of the number of days in a year
+	validates(:day_spread, { 
+		presence: true,
+		in: 1..364
+		})
+
+	validates(:per_day, { 
+		presence: true, 
+		})
+	validates(:category, { 
+		presence: true, 
+		length: { maximum: 50 }
+		})
+	validates(:description, { 
+		presence: true, 
+		length: { maximum: 100 }
+		})
+
 end
