7958ebf9af4eb071b73f8595469201b8de008511
Omair Ali <omairoa@gmail.com>
2014-10-14 23:18:04 +1100

    test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,33 +2,33 @@ class Transaction < ActiveRecord::Base
 
 	belongs_to :user
 	
-	validates(:amount, { 
-		presence: true, 
-		})
-	validates(:user_id, { 
-		presence: true, 
-		})
+# 	validates(:amount, { 
+# 		presence: true, 
+# 		})
+# 	validates(:user_id, { 
+# 		presence: true, 
+# 		})
 
-	validates(:date, { 
-		presence: true, 
-		})
+# 	validates(:date, { 
+# 		presence: true, 
+# 		})
 
-# Day spread has a max value of the number of days in a year
-	validates(:day_spread, { 
-		presence: true,
-		in: 1..364
-		})
+# # Day spread has a max value of the number of days in a year
+# 	validates(:day_spread, { 
+# 		presence: true,
+# 		in: 1..364
+# 		})
 
-	validates(:per_day, { 
-		presence: true, 
-		})
-	validates(:category, { 
-		presence: true, 
-		length: { maximum: 50 }
-		})
-	validates(:description, { 
-		presence: true, 
-		length: { maximum: 100 }
-		})
+# 	validates(:per_day, { 
+# 		presence: true, 
+# 		})
+# 	validates(:category, { 
+# 		presence: true, 
+# 		length: { maximum: 50 }
+# 		})
+# 	validates(:description, { 
+# 		presence: true, 
+# 		length: { maximum: 100 }
+# 		})
 
 end
