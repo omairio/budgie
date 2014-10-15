5c53880bae1339aadd2767855b066c1a7c1557f6
Omair Ali <omairoa@gmail.com>
2014-10-15 18:13:44 +1100

    display transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -8,17 +8,22 @@ class TransactionsController < ApplicationController
   def create
     @transaction = Transaction.new(transaction_params)
     @transaction.user_id = current_user.id
+
+    # Spread amount over a few days
     @type = 1
-    if(@spread_type == 1)
-      @type = 1
-    elsif (@spread_type == 2)
-      @type = 7
-    elsif (@spread_type == 3)
-      @type = 30
-    elsif (@spread_type == 4)
-      @type = 365
+    if (!@transaction.day_spread.nil?)
+      if(@transaction.spread_type == "Day")
+        @type = 1
+      elsif (@transaction.spread_type == "Week")
+        @type = 7
+      elsif (@transaction.spread_type == "Month")
+        @type = 30
+      elsif (@transaction.spread_type == "Year")
+        @type = 365
+      end
+      @transaction.per_day = @transaction.amount/(@transaction.day_spread * @type)
     end
-    @transaction.per_day = @transaction.amount/(@transaction.day_spread * @type)
+
     if (@transaction.save)
       redirect_to root_path
     else
@@ -40,6 +45,12 @@ class TransactionsController < ApplicationController
   end
 
   def edit
+    if (signed_in?)
+      post = Post.find(params[:id])
+      if (current_user.id == post.user_id)
+        post.destroy
+      end
+    end
   end
 
   def show
